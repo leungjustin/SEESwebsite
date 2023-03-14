5de9f4ce41b5d3d84a4eafd474862d4909364431
Owen-Lundell <owenlundell@gmail.com>
2023-03-14 15:07:24 -0700

    employee model

--- SEESwebsite/SEESwebsite/Models/Employee.cs
+++ SEESwebsite/SEESwebsite/Models/Employee.cs
@@ -1,6 +1,17 @@
-﻿namespace SEESwebsite.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SEESwebsite.Models
 {
-    public class Employee
+    public class Employee : IdentityUser
     {
+        public IList<string> RoleName { get; set; }    
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string SSN { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmployeeStatis { get; set; }
     }
 }
