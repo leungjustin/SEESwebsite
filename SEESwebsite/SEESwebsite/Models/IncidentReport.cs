d50115358990be3710bf09c8b528e8d5694d236a
Justin Leung <leungj3@my.lanecc.edu>
2023-03-14 15:29:50 -0700

    Added IncidentReport model

---
+++ SEESwebsite/SEESwebsite/Models/IncidentReport.cs
@@ -0,0 +1,13 @@
+﻿namespace SEESwebsite.Models
+{
+    public class IncidentReport
+    {
+        public int ReportId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public Event Event { get; set; }
+        public string Subject { get; set; }
+        public string ReportDetails { get; set; }
+        public bool WasReviewed { get; set; }
+    }
+}
