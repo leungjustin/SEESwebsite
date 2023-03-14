9adc69536fcfcec0b2bd66fe36d41d12f95294cb
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2023-03-14 15:15:59 -0700

    Update Shift.cs
    uncommented Employee

--- SEESwebsite/SEESwebsite/Models/Shift.cs
+++ SEESwebsite/SEESwebsite/Models/Shift.cs
@@ -9,6 +9,6 @@ namespace SEESwebsite.Models
         public DateTime ShiftStartTime { get; set; }
         public DateTime ShiftEndTime { get; set; }
         //public Event Event {get; set;} //Event not yet implemented
-        //public Employee Employee {get; set;} //Employee not yet implemented
+        public Employee Employee {get; set;} //Employee not yet implemented
     }
 }
