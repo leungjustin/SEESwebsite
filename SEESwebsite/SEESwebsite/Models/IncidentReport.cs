4345a84a7ab7762abfd21156b9e2382e0004411a
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2023-03-16 15:16:20 -0700

    Update IncidentReport.cs
    added key back to incident report to resolve conflicts

--- SEESwebsite/SEESwebsite/Models/IncidentReport.cs
+++ SEESwebsite/SEESwebsite/Models/IncidentReport.cs
@@ -1,7 +1,9 @@
-﻿namespace SEESwebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace SEESwebsite.Models
 {
     public class IncidentReport
     {
+        [Key]
         public int IncidentReportId { get; set; }
         public Employee Employee { get; set; }
         public DateTime IncidentDate { get; set; }
